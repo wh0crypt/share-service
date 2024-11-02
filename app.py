11da3ec5f5c42ea14072f25a346a3b7d4876ff8c
wh0crypt <wh0crypt@protonmail.com>
2024-11-02 13:35:26 +0000

    Add utils.py with check_env and apply it to app.py
    Signed-off-by: wh0crypt <wh0crypt@protonmail.com>

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import flask
 import os
 import sys
+import utils
 from dotenv import load_dotenv
 from typing import Tuple
 
@@ -8,23 +9,26 @@ app = flask.Flask(__name__)
 
 # Folder where uploaded files will be saved
 load_dotenv(".env")
+SITE_INDEX = os.getenv("SITE_INDEX")
 UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER")
-if UPLOAD_FOLDER is None:
-  print("UPLOAD_FOLDER not set in .env file")
-  sys.exit(1)
+UPLOAD_ROUTE = os.getenv("UPLOAD_ROUTE")
+UPLOADS_ROUTE = os.getenv("UPLOADS_ROUTE")
+
+ENV_VARS = (SITE_INDEX, UPLOAD_FOLDER, UPLOAD_ROUTE, UPLOADS_ROUTE)
+utils.check_env(ENV_VARS)
 
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER      
 
 # Route to show the main page
-@app.route('/')
-def index():
+@app.route(SITE_INDEX)
+def index() -> str:
   files = os.listdir(app.config['UPLOAD_FOLDER'])
   return flask.render_template('index.html', files=files)
 
 # Route to handle file uploads
-@app.route('/upload', methods=['POST'])
-def upload_file():
+@app.route(UPLOAD_ROUTE, methods=['POST'])
+def upload_file() -> flask.Response:
   if 'file' not in flask.request.files:
     return flask.redirect(flask.url_for('index'))
   file = flask.request.files['file']
@@ -35,8 +39,8 @@ def upload_file():
     return flask.redirect(flask.url_for('index'))
 
 # Route to see the uploaded files
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
+@app.route(f"{UPLOADS_ROUTE}/<filename>")
+def uploaded_file(filename) -> flask.Response:
   return flask.send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
---
+++ utils.py
@@ -0,0 +1,26 @@
+import sys
+from typing import Tuple
+
+def check_env(env_vars : Tuple[str]) -> None:
+  """
+  Checks if the specified environment variables are set.
+
+  This function takes a tuple of environment variable names and checks
+  if each one is set (not None). If any variable is not set, it prints
+  an error message and terminates the program with an exit code of 1.
+
+  Args:
+    env_vars (Tuple[str]): A tuple of environment variable names to check.
+
+  Returns:
+      None
+  """
+  error = False
+  
+  for env_var in env_vars:
+    if env_var is None:
+      print(f"{env_var} not set in .env file")
+      error = True
+  
+  if error:
+    sys.exit(1)
